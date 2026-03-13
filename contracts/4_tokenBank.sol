ac45867657ea6334f307fe0b06f57db65285e341
Pansy <pansyzzz1225@gmail.com>
2026-03-13 22:26:13 +0800

    实现紧急暂停，gas优化，增加查询函数

--- contracts/4_tokenBank.sol
+++ contracts/4_tokenBank.sol
@@ -4,81 +4,85 @@ pragma solidity ^0.8.30;
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./6_ITokenRecipient.sol";
 
-contract tokenBank is ITokenRecipient{
+contract tokenBank is ITokenRecipient, ReentrancyGuard, Ownable(msg.sender) {
     using SafeERC20 for IERC20;
     //用户地址=>代币地址=>金额
-    mapping(address => mapping(address => uint256)) userTokenBalance;
+    mapping(address => mapping(address => uint256)) private _userTokenBalance;
     //代币地址=>合约储备总金额
-    mapping(address => uint256) tokenTotalReserves;
+    mapping(address => uint256) private _tokenReserves;
+    //紧急暂停开关
+    bool private _paused; 
 
-    event depositEvent(address user,address token,uint256 amount);
-    event withdrawEvent(address to,address token,uint256 value);
-    event tokenReservesChangeEvent(address token,uint256 oldReserves,uint256 newReserves);
+    event depositEvent(address indexed user,address indexed token,uint256 amount);
+    event withdrawEvent(address indexed to,address indexed token,uint256 value);
+    event Paused(address account);
+    event Unpaused(address account);
 
-    error AmountNotZero();
-    error TokenTransferError();
-    error TokenBalanceNotEnough();
+    error ZeroAmount();
+    error InvalidTokenAddress();
+    error InsufficientBalance();
     error TransferAddressError();
-    error AddressNotToken();
+    error TransferFailed();
+    error InsufficientContractBalance();
+    error ContractPaused();
 
-    //存款
-    function deposit(address _token,uint256 _amount) public {
-        if( _amount==0 ) revert AmountNotZero();
-        if( _token==address(0) || _token.code.length==0 ) revert AddressNotToken();
+    // 暂停修饰器
+    modifier whenNotPaused() {
+        if (_paused) revert ContractPaused();
+        _;
+    }
+
+    // 紧急暂停（仅管理员）
+    function pause() external onlyOwner {
+        _paused = true;
+        emit Paused(msg.sender);
+    }
+
+    function unpause() external onlyOwner {
+        _paused = false;
+        emit Unpaused(msg.sender);
+    }
+
+
+    function deposit(address _token, uint256 _amount) public nonReentrant whenNotPaused {
+        if( _amount==0 ) revert ZeroAmount();
+        if( _token==address(0) || _token.code.length==0 ) revert InvalidTokenAddress();
         
         IERC20 token = IERC20(_token);
-        if( _amount > token.allowance(msg.sender,address(this)) ) revert TokenBalanceNotEnough();
-        
         token.safeTransferFrom(msg.sender, address(this), _amount);
 
-        _depositInformationChange(msg.sender, _token, _amount);
+        _userTokenBalance[msg.sender][_token] += _amount;
+        _tokenReserves[_token] += _amount;
 
+        emit depositEvent(msg.sender, _token, _amount);
     }
 
-    //存款-本地信息修改
-    function _depositInformationChange(address _user,address _token,uint256 _amount) private {
-        uint256 oldReserves = tokenTotalReserves[_token];
-        uint256 newReserves = IERC20(_token).balanceOf(address(this));
-        if( newReserves-oldReserves < _amount ) revert TokenTransferError();
+  
+    function withdraw(address _token,uint256 _amount) public nonReentrant whenNotPaused {
+        if( _amount==0 ) revert ZeroAmount();
+        if( _token==address(0) || _token.code.length==0 ) revert InvalidTokenAddress();
 
-        userTokenBalance[_user][_token]+=_amount;
-        emit depositEvent(_user, _token, _amount);
+        uint256 userBalance = _userTokenBalance[msg.sender][_token];
+        if (userBalance < _amount) revert InsufficientBalance();
 
-        tokenTotalReserves[_token]=newReserves;
-        emit tokenReservesChangeEvent(_token, oldReserves, newReserves);
-    }
-
-    //提款
-    function withdraw(address _token,uint256 _amount) public {
-        if(_amount==0) revert AmountNotZero();
-        if(_token==address(0) || _token.code.length==0 ) revert AddressNotToken();
-        
         IERC20 token = IERC20(_token);
-        if( _amount > userTokenBalance[msg.sender][_token] ) revert TokenBalanceNotEnough();
+
+        uint256 contractBalance = token.balanceOf(address(this));
+        if (contractBalance < _amount) revert InsufficientContractBalance();
 
         token.safeTransfer(msg.sender,_amount);
 
-        _withdrawInformationChange(msg.sender, _token, _amount);
+        _userTokenBalance[msg.sender][_token] -= _amount;
+        _tokenReserves[_token] -= _amount;
+
+        emit withdrawEvent(msg.sender, _token, _amount);
     }
 
-    //提款-本地信息修改
-    function _withdrawInformationChange(address _user,address _token,uint256 _amount) private {
-        uint256 oldReserves = tokenTotalReserves[_token];
-        uint256 newReserves = IERC20(_token).balanceOf(address(this));
-        if( oldReserves - newReserves < _amount ) revert TokenTransferError();
-
-        userTokenBalance[_user][_token]-=_amount;
-        emit withdrawEvent(_user, _token, _amount);
-
-        tokenTotalReserves[_token]=newReserves;
-        emit tokenReservesChangeEvent(_token, oldReserves, newReserves);
-
-    }
-
-    //离线签名授权
-    function permitDeposit(
+   function permitDeposit(
         address _token,
         address _owner,
         uint256 _value,
@@ -86,18 +90,41 @@ contract tokenBank is ITokenRecipient{
         uint8 _v,
         bytes32 _r,
         bytes32 _s
-    ) public {
+    ) external nonReentrant whenNotPaused {
+        if( _value==0 ) revert ZeroAmount();
+        if( _token==address(0) || _token.code.length==0 ) revert InvalidTokenAddress();
+        
         IERC20Permit(_token).permit(_owner,address(this),_value,_deadline,_v,_r,_s);
         deposit(_token, _value);
     }
 
-    //回调函数
-    function onTransferReceived(address _from,uint256 _amount) external {
-        if(_amount==0) revert AmountNotZero();
-        if(_from==address(0) || _from.code.length!=0 ) revert TransferAddressError();
-        if( msg.sender.code.length==0 ) revert AddressNotToken();
 
-        //更新本地信息
-        _depositInformationChange(_from, msg.sender, _amount);
+    function onTransferReceived(address _from,uint256 _amount) external nonReentrant whenNotPaused {
+        if( _amount==0 ) revert ZeroAmount();
+        if(_from==address(0) || _from.code.length!=0 ) revert TransferAddressError();
+        if( msg.sender.code.length==0 ) revert InvalidTokenAddress();
+
+        IERC20 token = IERC20(msg.sender);
+        uint256 contractBalance = token.balanceOf(address(this));
+        uint256 expectedReserve = _tokenReserves[msg.sender] + _amount;
+        if (contractBalance < expectedReserve) revert TransferFailed();
+
+        _userTokenBalance[_from][msg.sender] += _amount;
+        _tokenReserves[msg.sender] = expectedReserve;
+
+        emit depositEvent(_from, msg.sender, _amount);
+    }
+
+    // 查询函数
+    function getUserBalance(address _user, address _token) external view returns (uint256) {
+        return _userTokenBalance[_user][_token];
+    }
+
+    function getContractReserve(address _token) external view returns (uint256) {
+        return _tokenReserves[_token];
+    }
+
+    function getContractBalance(address _token) external view returns (uint256) {
+        return IERC20(_token).balanceOf(address(this));
     }
 }
